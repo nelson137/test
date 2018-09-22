5f1f89b801c1d59a99b63e191c67c0d8fe49c6ff
Nelson Earle <nelson.earle137@gmail.com>
2018-09-21 21:22:21 -0500

    removed 2 source code files

--- bin-src/c.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("from c.c\n");
-    return 0;
-}
--- bin-src/d.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("from d.c\n");
-    return 0;
-}
