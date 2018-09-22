34cd6fc7f416cc4a2d90cb35951634124903b0dc
Nelson Earle <nelson.earle137@gmail.com>
2018-09-21 20:57:25 -0500

    added test c files

---
+++ bin-src/b.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("from b.c\n");
+    return 0;
+}
---
+++ bin-src/e.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("from e.c\n");
+    return 0;
+}
